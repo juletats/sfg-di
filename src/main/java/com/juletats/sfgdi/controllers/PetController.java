ac67e4050c5b093f7028e5a78033de88de413b2b
juletats <juleta-tsaturyan@mail.ru>
2022-06-21 08:05:15 +0300

    Dependency Injection Task with Pet Service using @Profile & @Service

---
+++ src/main/java/com/juletats/sfgdi/controllers/PetController.java
@@ -0,0 +1,18 @@
+package com.juletats.sfgdi.controllers;
+
+import com.juletats.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
