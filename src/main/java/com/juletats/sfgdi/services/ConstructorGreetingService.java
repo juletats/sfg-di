426859af20f448d05417d6369b22c9de2db81028
juletats <juleta-tsaturyan@mail.ru>
2022-08-30 23:45:56 +0300

    Added Configuration class removed @Service, replaced it with beans in config class removed @Service, @Bean, @Primary and @Profile from services

--- src/main/java/com/juletats/sfgdi/services/ConstructorGreetingService.java
+++ src/main/java/com/juletats/sfgdi/services/ConstructorGreetingService.java
@@ -2,7 +2,7 @@ package com.juletats.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class ConstructorGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
--- src/main/java/com/juletats/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/com/juletats/sfgdi/services/PropertyInjectedGreetingService.java
@@ -2,7 +2,6 @@ package com.juletats.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class PropertyInjectedGreetingService implements  GreetingService{
 public String sayGreeting(){
     return "Hello world! - Property";
