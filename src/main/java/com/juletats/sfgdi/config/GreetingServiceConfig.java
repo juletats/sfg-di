329025306b9c99f61ccd73aa270b061857a2f328
juletats <juleta-tsaturyan@mail.ru>
2022-09-14 22:40:33 +0300

    Added XML Configuration. used ImportResource

--- src/main/java/com/juletats/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/juletats/sfgdi/config/GreetingServiceConfig.java
@@ -5,11 +5,9 @@ import com.juletats.pets.PetServiceFactory;
 import com.juletats.sfgdi.repositories.EnglishGreetingRepository;
 import com.juletats.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import com.juletats.sfgdi.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
     @Bean
@@ -53,7 +51,7 @@ public class GreetingServiceConfig {
         return new PrimaryGreetingService();
     }
 
-    @Bean
+//    @Bean
     ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
     }
