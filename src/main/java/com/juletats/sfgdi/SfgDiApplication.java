ee81403778ff44d35a8b37a4e44d968c12009859
juletats <juleta-tsaturyan@mail.ru>
2022-09-28 00:35:27 +0300

    Properties binding via @ConfigurationProperties

--- src/main/java/com/juletats/sfgdi/SfgDiApplication.java
+++ src/main/java/com/juletats/sfgdi/SfgDiApplication.java
@@ -1,5 +1,6 @@
 package com.juletats.sfgdi;
 
+import com.juletats.sfgdi.config.SfgConfiguration;
 import com.juletats.sfgdi.controllers.*;
 import com.juletats.sfgdi.datasource.FakeDataSource;
 import com.juletats.sfgdi.services.PrototypeBean;
@@ -48,10 +49,18 @@ public class SfgDiApplication {
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
 
+		System.out.println("----------- Fake Data Source-------------");
+
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getJdbcurl());
+
+		System.out.println("-----------------Config Props Bean-------------");
+		SfgConfiguration sfgConfiguration =ctx.getBean(SfgConfiguration.class);
+		System.out.println(sfgConfiguration.getUsername());
+		System.out.println(sfgConfiguration.getPassword());
+		System.out.println(sfgConfiguration.getJdbcurl());
 	}
 
 }
